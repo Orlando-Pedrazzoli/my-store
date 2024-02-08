df7df28b94ca04aeeb449066f5412f43001c6cf6
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-02-08 10:38:35 +0000

    Components and Pages content created

--- frontend-store/src/main.jsx
+++ frontend-store/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ShopContextProvider from './Context/ShopContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
     </Router>
   </React.StrictMode>
 );
